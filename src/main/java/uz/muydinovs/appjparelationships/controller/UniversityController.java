f54003a430ce1b6a97ddb0034e9f90f399adb64a
johnUfo <muydinovumidjon17@gmail.com>
2024-09-17 23:14:53 +0500

    break time

--- src/main/java/uz/muydinovs/appjparelationships/controller/UniversityController.java
+++ src/main/java/uz/muydinovs/appjparelationships/controller/UniversityController.java
@@ -2,22 +2,46 @@ package uz.muydinovs.appjparelationships.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import uz.muydinovs.appjparelationships.entity.Address;
 import uz.muydinovs.appjparelationships.entity.University;
+import uz.muydinovs.appjparelationships.payload.UniversityDto;
+import uz.muydinovs.appjparelationships.repository.AddressRepository;
 import uz.muydinovs.appjparelationships.repository.UniversityRepository;
 
 import java.util.List;
 
 @RestController()
 public class UniversityController {
+    UniversityRepository universityRepository;
+    AddressRepository addressRepository;
+
 
     @Autowired
-    UniversityRepository universityRepository;
+    public UniversityController(UniversityRepository universityRepository, AddressRepository addressRepository) {
+        this.universityRepository = universityRepository;
+        this.addressRepository = addressRepository;
+    }
 
     @RequestMapping(value = "/university",method = RequestMethod.GET)
     public List<University> getUniversities() {
         return universityRepository.findAll();
+    }
+
+    @RequestMapping(value = "/university",method = RequestMethod.POST)
+    public String addUniversity(@RequestBody UniversityDto universityDto) {
+        University university = new University();
+        Address address = new Address();
+        address.setCity(universityDto.getCity());
+        address.setDistrict(universityDto.getDistrict());
+        address.setStreet(universityDto.getStreet());
+        university.setName(universityDto.getName());
+        university.setAddress(address);
+        addressRepository.save(address);
+        universityRepository.save(university);
+        return "Success";
     }
 }
